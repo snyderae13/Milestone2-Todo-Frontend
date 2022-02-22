adabfc7c676f0830ed8625c657a752fb18c885dc
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-02-21 19:07:53 -0600

    Built sorting todos by priority in TaskList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,76 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import TaskGroup from './TaskGroup';
+import TodoDataService from '../services/todoService.js'
 
 const TaskList = (props) => {
-    let groupTaskList = [];
+    // Use State for data pulled from database
+    let [todoData, setTodoData] = useState([]);
 
-    // Map on data to sort by priority or date <TaskGroup />
+    // Retrieve data from the database then set the state 
+    useEffect(() => {
+        TodoDataService.getAll().then(response => {setTodoData(response.data)})
+    }, [])
+
+    // Accessing props that were passed in
+    const priorities = props.priorities;
+    const dueDates = props.dueDates;
+
+    // Test data
+    let data = [{_id: "", name: "Test Data in TaskList.js", priority: "High", dueDate: "", description: ""}]
+    
+    // Need to fill groupTodoList whether sorting by priority or dueDate
+    let groupTodosList = [];
+
+    // Determine that sorting by priorities
+    if (priorities) {
+        let criticalTodos = [];
+        let highTodos = [];
+        let mediumTodos = [];
+        let lowTodos = [];
+
+        // Function to sort todos by priority and push them to an array of the same type
+        const sortByPriority = (todo) => {
+            if (todo.priority === "Critical") {
+                criticalTodos.push(todo);
+            } else if (todo.priority === "High") {
+                highTodos.push(todo);
+            } else if (todo.priority === "Medium") {
+                mediumTodos.push(todo);
+            } else {
+                lowTodos.push(todo);
+            }
+        }
+
+        // Iterating through all of the todos and using the sorting function
+        for (let i = 0; i < todoData.length; i++) {
+            let todo = todoData[i];
+            sortByPriority(todo);
+        }
+
+        // Creating an array of arrays, where each inner array represents a specific priority
+        let sortedTodos = [criticalTodos, highTodos, mediumTodos, lowTodos];
+
+        // Defining the headers to be used when sorting by priority
+        const priorityHeaders = [
+            'Critical: Do this task and ignore everything else!', 
+            'High: Needs to be completed soon', 
+            'Medium: No rush to be completed', 
+            'Low: Just a reminder for now'
+        ]
+
+        // Making a TaskGroup by priority and passing in the header and appropriate data as props
+        groupTodosList = priorityHeaders.map((priority, index) => {
+            data = sortedTasks[index];
+            return (
+                <TaskGroup key={index} header={priority} data={data} />
+            )
+        })
+    }
 
     return(
         <div>
-            <div>TaskList</div>
-            {groupTaskList}
+            <div>TodoList</div>
+            {groupTodosList}
         </div>
         
     )
