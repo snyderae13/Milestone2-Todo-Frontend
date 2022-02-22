37fe7e03bd3ca8e6375b93bcc22f2dc52a53b327
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-02-21 22:48:25 -0600

    Removed placeholder text and built TaskRow

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {Button} from 'react-bootstrap';
 
 const DeleteButton = (props) => {
-    return(
-        <div>DeleteButton</div>
+    let id = props.id
+
+    let delBtnStyle = {
+        padding: "0rem 0.25rem",
+        marginLeft: "0.25rem"
+    }
+
+    return (
+            <Button 
+                style={delBtnStyle} className="btn-danger"
+                ><i className="fas fa-trash-alt"></i>
+            </Button>
     )
 }
 
