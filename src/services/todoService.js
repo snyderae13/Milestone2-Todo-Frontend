eee7d680a58ff10150bcf147eae048d6e3f87744
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-02-26 13:08:22 -0600

    Changed paths in todoService.js to use heroku app as the backend

--- src/services/todoService.js
+++ src/services/todoService.js
@@ -2,24 +2,24 @@ import axios from 'axios';
 
 class TodoDataService {
     getAll = () => {
-        return axios.get('http://localhost:3456/todos')
+        return axios.get('https://todolistmilestone2.herokuapp.com/todos')
     }
 
     getTodo = (id) => {
-        return axios.get(`http://localhost:3456/todos/${id}`)
+        return axios.get(`https://todolistmilestone2.herokuapp.com/todos/${id}`)
     }
 
     createTodo = (data) => {
-        return axios.post('http://localhost:3456/todos/new', data)
+        return axios.post('https://todolistmilestone2.herokuapp.com/todos/new', data)
         
     }
 
     updateTodo = (id, data) => {
-        return axios.put(`http://localhost:3456/todos/${id}`, data)
+        return axios.put(`https://todolistmilestone2.herokuapp.com/${id}`, data)
     }
 
     deleteTodo = (id) => {
-        return axios.delete(`http://localhost:3456/todos/${id}`)
+        return axios.delete(`https://todolistmilestone2.herokuapp.com/${id}`)
     }
 
 }
